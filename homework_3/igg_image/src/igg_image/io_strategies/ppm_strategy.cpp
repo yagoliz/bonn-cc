09c7456956aaa9849a3af63ca31215bd49d3a684
Yago Lizarribar <yago.lizarribar@gmail.com>
2018-11-07 23:38:58 +0800

    Add ppm strategy

---
+++ homework_3/igg_image/src/igg_image/io_strategies/ppm_strategy.cpp
@@ -0,0 +1,96 @@
+#include "igg_image/io_strategies/ppm_strategy.h"
+
+namespace igg {
+
+  bool PpmIoStrategy::Write(const std::string& file_name, 
+                            const ImageData& data) const {
+
+    std::ofstream output(file_name);
+
+    // Fill the header
+    output << "P3 " << std::endl;
+    output << data.rows << " " << data.cols << " " << std::endl;
+    output << data.max_val << " " << std::endl;
+
+    // Fill the lines (We'll use 70 pixels per line)
+    int r = 0;
+    int c = 0;
+    int rows = data.rows;
+    int cols = data.cols;
+    while (r < rows) {
+      for (int i = 0; i < 5; i++) {
+        int idx = r * cols + c;
+        output << data.data[0][idx] << " " << data.data[1][idx] 
+               << " " << data.data[2][idx] << "\t";
+
+        if (++c == cols) {
+          r++;
+          c = 0;
+        }
+      }
+      output << std::endl;
+    }
+
+    return true;
+  }
+
+  ImageData PpmIoStrategy::Read(const std::string& file_name) const {
+    // Start file reader
+    std::ifstream input(file_name, std::ios_base::in);
+
+    // Create ImageData structure and the red, green and blue vectors
+    ImageData image;
+
+    // Read the header
+    std::string line;
+    bool read_first = false;
+    bool read_second = false;
+    bool read_third = false;
+    while(!read_third) {
+      std::getline(input, line);
+
+      // Check if first line
+      if (line[0] == 'P') {read_first = true; continue;}
+      // Check if comment
+      if (line[0] == '#') continue;
+      // Check if second line
+      if (read_first && !read_second) {
+        std::stringstream ss(line);
+        ss >> image.rows >> image.cols;
+        read_second = true;
+        continue;
+      }
+      if (read_second) {
+        std::stringstream ss(line);
+        ss >> image.max_val;
+        read_third = true;
+      }
+    }
+
+    // Values
+    std::vector<int> red(image.rows * image.cols, 0);
+    std::vector<int> green(image.rows * image.cols, 0);
+    std::vector<int> blue(image.rows * image.cols, 0);
+
+    // Read values
+    int cols = image.cols;
+    int r = 0;
+    int c = 0;
+    while(std::getline(input, line)) {
+      std::stringstream ss(line);
+
+      // I think there has to be a better solution
+      int idx = r * cols + c;
+      while (ss >> red[idx] >> green[idx] >> blue[idx]) {
+        if (++c == cols) {
+          r++;
+          c = 0;
+        }
+        idx = r * cols + c;
+      }
+    }
+    image.data = {red, green, blue};
+    return image;
+  }
+
+} // namespace igg
--- homework_3/igg_image/src/main.cpp
+++ homework_3/igg_image/src/main.cpp
@@ -4,6 +4,7 @@
 #include "igg_image/image.h"
 // #include <png++/png.hpp>
 #include "igg_image/io_strategies/png_strategy.h"
+#include "igg_image/io_strategies/ppm_strategy.h"
 
 int main() {
   // Let's check the PNG strategy
@@ -41,6 +42,31 @@ int main() {
   std::string file_name_big = "../data/riverbig.png";
   image.UpScale(4);
   image.WriteToDisk(file_name_big);
+
+// --------------------------------------------------------
+  // Time to test PPM strategy
+  std::string file_ppm = "../data/pbmlib.ascii.ppm";
+  igg::PpmIoStrategy strategy_ppm; 
+
+  // Create ppm image object
+  igg::Image image_ppm{strategy_ppm};
+
+  // Read the file 
+  if (!(image_ppm.ReadFromDisk(file_ppm))) {
+    std::cout << "Error reading file" << std::endl;
+    return 1;
+  }
+
+  // Change the image
+  std::string file_ppm_red = "../data/pbmlib.ascii.red.ppm";
+  for (int i = 0, rows = image_ppm.rows(); i < rows; i++) {
+    for (int j = 0, cols = image_ppm.cols(); j < cols; j++) {
+      igg::Image::Pixel p {image_ppm.at(i, j)};
+      image_ppm.at(i, j) = igg::Image::Pixel{p.red, p.green, p.blue};
+    }
+  }
+
+  image_ppm.WriteToDisk(file_ppm_red);
 
   // End of main
   return 0;
